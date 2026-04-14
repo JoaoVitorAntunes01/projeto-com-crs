6579049c37a0bc8f5f986a1c29426e178b94352f
JoãoVitorAntunes01 <joaovitorantunespc1@gmail.com>
2026-04-14 17:33:05 -0300

    atividadegerenciamento

---
+++ src/main/java/com/senai/gerenciamento/model/FuncionarioDTO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senai.gerenciamento.model;
+
+/**
+ *
+ * @author Aluno
+ */
+public class FuncionarioDTO {
+    private int id;
+    private String nome;
+    private String cargo;
+    private String departamento;
+    private String email;
+    private date data_contratacao;
+}
+
+ 
